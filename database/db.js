92b6a1e2062df75cfa1b41839bb6b5de82c00c4b
diki6969 <120658441+diki6969@users.noreply.github.com>
2023-02-07 18:38:23 +0700

    Update db.js
    Cok

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = 'https://api.sekha.me';
+var url = 'mongodb+srv://Ikyy556:KucingLari16@ikyy.qwyawka.mongodb.net/?retryWrites=true&w=majority';
 try {
 if(url == 'https://api.sekha.me') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
 } catch (e) {
